587d914d2c52674f7ad63166eb897908bd6479ea
nvietanh1909 <nvietanh.190904@example.com>
2024-11-25 14:49:15 +0700

    Add project files.

---
+++ HoaDon/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using HoaDon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HoaDon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult HoaDon()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult HoaDon(MD_ThongTinHoaDon hoadon)
+        {
+            if (ModelState.IsValid)
+            {
+               
+            }
+            return View();
+        }
+
+    }
+}
---
+++ HoaDon/Models/MD_ThongTinHoaDon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoaDon.Models
+{
+    public class MD_ThongTinHoaDon
+    {
+        public HoaDon HoaDon { get; set; }
+        public DonHang DonHang { get; set; }
+        public KhachHang KhachHang { get; set; }
+    }
+}
